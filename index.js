758c2cc4420ab53cd7f46d3ffaaad0ce24d889e4
rantaudev <admin@rantau.dev>
2020-07-11 15:00:46 +0700

    Add main execution context index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+const memberRouter = require("./src/routes/member.js");
+
+dotenv.config();
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+memberRouter(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
