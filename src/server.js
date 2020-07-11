398dd9af23935b578487ae6ed2962af380e0351c
RizkyArifNur <rizkyarifnur@gmail.com>
2020-07-11 20:59:50 +0700

    implement new member router and fix dotenv config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const dotenv = require('dotenv');
-const memberRouter = require("./src/routes/member.js");
-
+const dotenv = require("dotenv");
 dotenv.config();
+const memberRouter = require("./routes/member.js");
+
 const app = express();
 
 // parse requests of content-type: application/json
@@ -12,7 +12,8 @@ app.use(bodyParser.json());
 // parse requests of content-type: application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-memberRouter(app);
+// attach member router
+app.use("/members", memberRouter);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 4444;
